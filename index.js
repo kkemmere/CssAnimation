4cb7ca15ca97f929a79d788b7c4d99d311095852
Kevin <56128551+kkemmere@users.noreply.github.com>
2022-11-07 23:50:39 -0600

    Add files via upload

---
+++ index.js
@@ -0,0 +1,30 @@
+// Kevin Kemmerer
+// Happy Birthday stuff
+
+const colors = [
+    "color: blue",
+    "color: red",
+    "color: green",
+    "color: AliceBlue",
+    "color: Aqua",
+    "color: BlueViolet",
+    "color: Chartreuse",
+    "color: CadetBlue",
+    "color: Coral",
+    "color: Crimson",
+    "color: DarkGoldenRod",
+    "color: DarkMagenta",
+    "color: Gold",
+    "color: HotPink",
+    "color: IndianRed"
+
+];
+
+document.addEventListener("DOMContentLoaded", function randomColor() { 
+    setInterval(function () {
+        let color = colors[Math.floor(Math.random()*colors.length)];
+        document.getElementById("headingB").style = color;
+    }, 40);
+  });
+
+//document.getElementById("headingB").style = "color: blue";
